cf6c4d1c1ab381d14a3f02c551b16ad1c7e2dc97
Otter <otc@otterthecat.com>
2015-02-28 12:10:17 -0600

    added unit tests. need to update jscs & lint rules

---
+++ test/specs/promptosaurusTest.js
@@ -0,0 +1,158 @@
+/* jshint expr: true */
+// assertion library
+// /////////////////////////////////////////////////////////
+var chai = require('chai');
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+chai.should();
+chai.use(sinonChai);
+
+// stubs
+// /////////////////////////////////////////////////////////
+
+
+
+// modules to test
+// /////////////////////////////////////////////////////////
+var Rawr = require('../../lib/promptosaurus');
+var rawr;
+
+describe('promptosaurus', function () {
+	'use strict';
+
+	beforeEach(function (){
+		rawr = new Rawr();
+	});
+
+	afterEach(function (){
+		rawr = null;
+	});
+
+	describe('#add()', function(){
+		'use strict';
+
+		var fakeString = 'foo';
+		var fakeFunc = function () {};
+
+		it('should update internal queue using passed arguments', function (){
+			rawr.add(fakeString, fakeFunc);
+			rawr.queue[0].query.should.contain(fakeString);
+			rawr.queue[0].callback.should.equal(fakeFunc);
+		});
+
+		it('should return promptosaurus instance', function(){
+			var returnValue = rawr.add(fakeString, fakeFunc);
+			returnValue.should.equal(rawr);
+		});
+	});
+
+	describe('#askNext()', function(){
+		describe('when queue property\'s length is > 0', function(){
+
+			var fakeString = 'foo';
+			var fakeFunc = function () {};
+
+			it('should update counter property', function(){
+				rawr.counter.should.equal(0);
+				rawr.queue[0] = {
+					'query': fakeString,
+					'callback': fakeFunc
+				}
+
+				rawr.askNext();
+				rawr.counter.should.equal(1);
+			});
+
+			it('should remove item from queue', function(){
+				rawr.queue[0] = {
+					'query': fakeString,
+					'callback': fakeFunc
+				}
+				rawr.queue.length.should.equal(1);
+				rawr.askNext();
+				rawr.queue.length.should.equal(0);
+			});
+
+			it('should present question', function(){
+
+			});
+		});
+
+		describe('when queue property\'s length is <= 0', function(){
+			it('should set counter property to 0', function(){
+				rawr.counter = 1;
+				rawr.askNext();
+				rawr.counter.should.equal(0)
+			});
+
+			it('should call assigned #complete() callback', function(){
+				var completeStub = sinon.stub(rawr, 'complete');
+				rawr.askNext();
+				completeStub.should.have.been.calledOnce;
+				completeStub.should.have.been.calledWith(rawr.inputs);
+
+				rawr.complete.restore();
+			});
+
+			it('should display farewell message', function(){
+				var consoleStub = sinon.stub(console, 'log');
+				rawr.askNext();
+				console.log.restore();
+				consoleStub.should.have.been.calledOnce;
+				consoleStub.should.have.been.calledWith(rawr.farewell);
+			});
+
+			it('should close readline interface', function(){
+				var closeStub = sinon.stub(rawr.line, 'close');
+				rawr.askNext();
+				closeStub.should.have.been.calledOnce;
+			});
+		});
+	});
+
+	describe('#getQHandler()', function(){
+		var fakeObj = {
+			callback: function(){}
+		};
+
+		it('should return a function', function(){
+			var returnValue = rawr.getQHandler(fakeObj);
+			returnValue.should.be.a('function');
+		});
+	});
+
+	describe('#ask()', function(){
+
+		it('should display greeting', function(){
+			var consoleStub = sinon.stub(console, 'log');
+			rawr.ask()
+			console.log.restore();
+
+			consoleStub.should.have.been.calledWith(rawr.greeting);
+
+		});
+
+		it('should start the prompt process', function() {
+			var nextStub = sinon.stub(rawr, 'askNext');
+			rawr.ask();
+
+			nextStub.should.have.been.calledOnce;
+			rawr.askNext.restore();
+		});
+	});
+
+	describe('#done()', function (){
+
+		var callback = function (){};
+
+		it('should set .complete property as passed callback', function (){
+			rawr.done(callback);
+			rawr.complete.should.equal(callback);
+		});
+
+		it('should return promptosaurus instance', function (){
+			var returnValue = rawr.done(callback);
+			returnValue.should.equal(rawr);
+		});
+	});
+});
